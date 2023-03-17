e2bfb782bce62b4753f0d9de00e4bef5c24dd05c
ten <fernandez.kirstenpearl@auf.edu.ph>
2023-03-17 10:45:04 +0800

    module 3 activity 1 to 4

---
+++ module03_activity/module03_activity/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace module03_activity
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Activity01(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity1());
+        }
+
+        private async void Activity02(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity2());
+        }
+        private async void Activity03(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity3());
+        }
+
+        private async void Activity04(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity4());
+        }
+    }
+}
---
+++ module03_activity/module03_activity/activity1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace module03_activity
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity1 : ContentPage
+    {
+        public activity1()
+        {
+            InitializeComponent();
+        }
+    }
+}
